82fde2b6ec1070d6931f83f746104e24cb532d99
Andy <aamesa03@gmail.com>
2023-11-19 16:52:56 -0500

    adding erd template

---
+++ app/seats4u/src/boundary/Boundary.js
@@ -0,0 +1,8 @@
+
+
+/* Redraw entire canvas from model */
+export function redrawCanvas(model, canvasObj, appObj) {
+  const ctx = canvasObj.getContext('2d');
+  if (ctx === null) { return; }    // here for testing purposes..
+
+}
---
+++ app/seats4u/src/model/Model.js
@@ -0,0 +1,8 @@
+/* represents the the logic for the entire model (board, counters, and butttons) */
+export default class Model {
+
+    constructor(which) {
+
+    }
+
+}
