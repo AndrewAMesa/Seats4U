78ae440ff3aab32d4bad841be84a26f161535ef3
Timothy Rinaldi <93170012+terinaldi2024@users.noreply.github.com>
2023-11-28 14:52:55 -0500

    Venue inputs work with lambda response on token!
    Co-authored-by: lcsanneman <lcsanneman@users.noreply.github.com>

--- app/seats4u/src/Pages/Home.js
+++ app/seats4u/src/Pages/Home.js
@@ -1,24 +1,59 @@
 import React from 'react';
+import { createVenueVMCon } from './controller/CreateVenueVMCon';
+
+var token;
+var venueName;
+var location;
+var defaultPrice;
+var numRows;
+var leftCols;
+var centerCols;
+var rightCols;
 
 const Home = () => {
-  const [token, setVal] = React.useState();
-  const change = event => {
-    const newvalue = event.target.value
-    setVal(event.target.value)
-  }
 
   return (
     <div className="Home">
       <h1>Home</h1>
       <button className='createVenue' data-testid="createVenue" onClick={(e) => createVenue()} >Create Venue</button>
       <button className='deleteVenue' data-testid="deleteVenue" onClick={(e) => deleteVenue()} >Delete Venue</button>
-      <input onChange={change} value={token} placeholder='type token here' />
+
+      <input id='token' value={token} placeholder='token' />
+      <input id='venueName' value={venueName} placeholder='venue name' />
+      <input id='location' value={location} placeholder='location' />
+      <input id='defaultPrice' value={defaultPrice} placeholder='default price' />
+      <input id='numRows' value={numRows} placeholder='#rows' />
+      <input id='leftCols' value={leftCols} placeholder='#left columns' />
+      <input id='centerCols' value={centerCols} placeholder='#center columns' />
+      <input id='rightCols' value={rightCols} placeholder='#right columns' />
 
     </div>
   );
 };
 
 function createVenue() {
+  if (document.getElementById('location').value != "" &&
+    document.getElementById('defaultPrice').value != "" && document.getElementById('numRows').value != "" &&
+    document.getElementById('leftCols').value != "" && document.getElementById('centerCols').value != "" &&
+    document.getElementById('rightCols').value != "" && document.getElementById('venueName').value != "") {
+
+    console.log("venue name: " + document.getElementById('venueName').value);
+    console.log("location: " + document.getElementById('location').value);
+    console.log("default price: " + document.getElementById('defaultPrice').value);
+    console.log("numRows: " + document.getElementById('numRows').value);
+    console.log("leftCols: " + document.getElementById('leftCols').value);
+    console.log("centerCols: " + document.getElementById('centerCols').value);
+    console.log("rightCols: " + document.getElementById('rightCols').value);
+
+    //run controller for VenueVMCon
+    createVenueVMCon();
+
+  }
+  else {
+    console.log("error");
+  }
+
+
 }
 
 function deleteVenue() {
--- app/seats4u/src/Pages/controller/CreateVenueVMCon.js
+++ app/seats4u/src/Pages/controller/CreateVenueVMCon.js
@@ -1,20 +1,21 @@
-import { post } from "/Api"
+import { post } from "./Api"
 
-export function addTwoNumbers() {
+export function createVenueVMCon() {
     let venueName = document.getElementById("venueName")
     let location = document.getElementById("location")
     let defaultPrice = document.getElementById("defaultPrice")
     let rows = document.getElementById("numRows")
     let leftColumns = document.getElementById("leftCols")
     let centerColumns = document.getElementById("centerCols")
-    let rightColumns = document.getElementById("righCols")
+    let rightColumns = document.getElementById("rightCols")
 
     // prepare payload for the post
     let data = { 'name': venueName.value, 'location': location.value, 'defaultPrice': defaultPrice.value, 
     'rows': rows.value, 'leftColumns': leftColumns.value, 'centerColumns': centerColumns.value, 'rightColumns': rightColumns.value }
 
-    const handler = (json) => {
-        document.getElementById("token").value = json.token
+    const handler = (response) => {
+        document.getElementById("token").value = JSON.parse(response.token)
+        console.log(JSON.parse(response.token))
     }
 
     post('/createVenueVM', data, handler)
