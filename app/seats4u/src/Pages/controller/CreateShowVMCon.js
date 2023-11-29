d2c0dae3efad1393b3b51ac2fad14a3eb0c8a1fe
Requiem-Requin <bmpodlipec@gmail.com>
2023-11-29 11:30:48 -0500

    added delete and list venue options
    added deleteVenueVM, listVenuesVM, and listVenueVMCon, and deleteVenueVMcon

---
+++ app/seats4u/src/Pages/controller/CreateShowVMCon.js
@@ -0,0 +1,36 @@
+// Importing the 'post' function from the "./Api" module
+import { post } from "./Api";
+
+// Function to create a Show VM (View Model)
+export function createShowVMCon() {
+    // Retrieving input values from HTML elements
+    let venueToken = document.getElementById("token");
+    let venueName = document.getElementById("venueName");
+    let showName = document.getElementById("showName");
+    let showDate = document.getElementById("showDate");
+    let showTime = document.getElementById("showTime");
+    let defaultPrice = document.getElementById("defaultPrice");
+
+    // Prepare payload for the post request
+    let data = {
+        'venueToken': venueToken.value,
+        'venueName': venueName.value,
+        'showName': showName.value,
+        'showDate': showDate.value,
+        'showTime': showTime.value,
+        'defaultPrice': defaultPrice.value
+    };
+
+    // Callback function to handle the response from the server
+    const handler = (response) => {
+        // Logging the parsed response status code to the console
+        if (JSON.parse(response.statusCode) == "200") {
+            console.log(JSON.parse(response.statusCode));
+        } else {
+            document.getElementById("token").value = JSON.parse(response.error);
+        }
+    };
+
+    // Making a POST request to the '/createShowVM' endpoint with the prepared data and the callback function
+    post('/createShowVM', data, handler);
+}
