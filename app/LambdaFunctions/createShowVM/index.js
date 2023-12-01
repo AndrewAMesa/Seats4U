d77ded06bb3b81b065a4794bbffddd762ce59f40
Andy <aamesa03@gmail.com>
2023-12-01 14:07:00 -0500

    editing shows lambda function

--- app/LambdaFunctions/createShowVM/index.js
+++ app/LambdaFunctions/createShowVM/index.js
@@ -3,7 +3,7 @@ const db_access = require('/opt/nodejs/db_access');
 
 exports.handler = async (event) => {
 
-    // get credentials from the db_access layer (loaded separately via AWS console)
+    // Get credentials from the db_access layer (loaded separately via AWS console)
     var pool = mysql.createPool({
         host: db_access.config.host,
         user: db_access.config.user,
@@ -13,16 +13,17 @@ exports.handler = async (event) => {
 
     let errorMessage = "error";
 
-    console.log(event.token)
+    // Log the token from the event
+    // console.log(event.token)
 
-    // validates if that token already exists
+    // Validates if that token already exists
     let tokenAndVenueExists = (token, name) => {
         return new Promise((resolve, reject) => {
             pool.query("SELECT * FROM Venues WHERE venueToken=? && venueName=?", [token, name], (error, rows) => {
                 if (error) {
                     return reject(error);
                 }
-                console.log(rows)
+                // console.log(rows)
                 if ((rows) && (rows.length >= 1)) {
                     return resolve(true);
                 } else {
@@ -33,14 +34,14 @@ exports.handler = async (event) => {
         });
     };
 
-    // validates if that show already exists
-    let showExists = (showName, date, time) => {
+    // Validates if that show already exists
+    let showExists = (showName, date, time, venueName) => {
         return new Promise((resolve, reject) => {
-            pool.query("SELECT * FROM Shows WHERE showName=? AND showDate=? AND showTime=?", [showName, date, time], (error, rows) => {
+            pool.query("SELECT * FROM Shows WHERE showName=? AND showDate=? AND showTime=? AND venueName=?", [showName, date, time, venueName], (error, rows) => {
                 if (error) {
                     return reject(error);
                 }
-                console.log(rows)
+                // console.log(rows)
                 if ((rows) && (rows.length >= 1)) {
                     errorMessage = "Exact show already exists at venue"
                     return resolve(true);
@@ -50,25 +51,52 @@ exports.handler = async (event) => {
             });
         });
     };
-    
 
     let response = undefined;
     const validToken = await tokenAndVenueExists(event.venueToken, event.venueName);
-    const alreadyExists = await showExists(event.showName, event.showDate, event.showTime);
-    console.log("checking")
-    
+    const alreadyExists = await showExists(event.showName, event.showDate, event.showTime, event.venueName);
+    // console.log("checking")
+
+    let tempShowID = 0;
+
     // If the show doesn't exist and the token is valid, add the show to the database
     if (!alreadyExists && validToken) {
+        // console.log("in loop")
 
-        // adds show to database
+                // Finds the venue 
+        let findVenueSize = (venueName) => {
+            return new Promise((resolve, reject) => {
+                pool.query("SELECT region, rowNum, colNum FROM Sections WHERE venueName=?", [venueName], (error, rows) => {
+                    if (error) {
+                        return reject(error);
+                    }
+                    if (rows) {
+                        return resolve(rows);
+                    } else {
+                        return resolve(false);
+                    }
+                });
+            });
+        }
+
+        let size = await findVenueSize(event.venueName);
+        const columnCount = size.reduce((sum, row) => sum + row.colNum, 0);
+        const rowCount = size[0].rowNum;
+        const totalSeats = columnCount * rowCount
+
+        // Adds show to the database
         let createShow = (showName, showDate, showTime, price, venueName) => {
             return new Promise((resolve, reject) => {
-                pool.query("INSERT into Shows(showName, showDate, showTime, defaultPrice, venueName) VALUES(?,?,?,?,?);", [showName, showDate, showTime, price, venueName], (error, rows) => {
+                pool.query("INSERT into Shows(showName, showDate, showTime, defaultPrice, availableSeatsCounter, venueName) VALUES(?,?,?,?,?,?);", [showName, showDate, showTime, price, totalSeats, venueName], (error, rows) => {
                     if (error) {
                         return reject(error);
                     }
                     if ((rows) && (rows.affectedRows >= 1)) {
+                        // console.log(rows)
+                        tempShowID = rows.insertId;
+                        // console.log(rows)
                         return resolve(true);
+
                     } else {
                         return resolve(false);
                     }
@@ -77,8 +105,35 @@ exports.handler = async (event) => {
         }
 
         // Execute the show creation function
-        let venueCreationResult = await createShow(event.showName, event.showDate, event.showTime, event.defaultPrice, event.venueName)
-        if (venueCreationResult == true){
+        let venueCreationResult = await createShow(event.showName, event.showDate, event.showTime, event.defaultPrice, event.venueName);
+
+        // Adds seats to the database
+        let addSeat = (showID, rowNum, columnNum) => {
+            return new Promise((resolve, reject) => {
+                pool.query("INSERT into Seats(showID, rowNum, colNum) VALUES(?,?,?);", [showID, rowNum, columnNum], (error, rows) => {
+                    if (error) {
+                        return reject(error);
+                    }
+                    if (rows) {
+                        // console.log("did it")
+                        return resolve(true);
+
+                    } else {
+                        return resolve(false);
+                    }
+                });
+            });
+        }
+
+        // console.log(columnCount)
+        // Loop to add seats
+        for (let i = 0; i < columnCount; i++) {
+            for (let j = 0; j < rowCount; j++) {
+                await addSeat(tempShowID, j, i);
+            }
+        }
+
+        if (venueCreationResult == true) {
             response = {
                 statusCode: 200
             }
