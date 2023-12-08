105476b9e6f92e28017f5d8c6bf35cd341308ce7
Brooke Podlipec <bmpodlipec@gmail.com>
2023-12-08 14:51:42 -0500

    searchShows updated

--- app/seats4u/src/Pages/controller/SearchShowsCCon.js
+++ app/seats4u/src/Pages/controller/SearchShowsCCon.js
@@ -32,11 +32,25 @@ export function searchShowsCCon(type) {
             //console.log(JSON.parse(response.statusCode));
             let list = "";
             for (let i = 0; i < response.shows.length; i++) {
-                if (response.shows[i].soldOut == 1) {
-                list = list + "show name: " + response.shows[i].showName + " | time: " + response.shows[i].showTime + " | date:  " + response.shows[i].showDate + " | <mark>sold out</mark> | " + "showID: " + response.shows[i].showID + "<br>";
+
+                // Convert SQL date and time strings to JavaScript Date object
+                //const dateString = response.venues[i].showDate;
+                const mysqlDate = response.shows[i].showDate;
+                const dateObject = new Date(mysqlDate);
+
+                // Extract day, month, and year
+                const day = dateObject.getDate();
+                const month = dateObject.getMonth() + 1; // Months are zero-indexed, so we add 1
+                const year = dateObject.getFullYear();
+
+                // Create a formatted date string
+                const dateString = `${month}-${day}-${year}`;
+
+                if (type = 0 && response.shows[i].soldOut == 1) {
+                list = list + "show name: " + response.shows[i].showName + " | time: " + response.shows[i].showTime + " | date:  " + dateString + " | <mark>sold out</mark> | " + "showID: " + response.shows[i].showID + "  | venue:" + response.shows[i].venueName + "<br>";
                 }
                 else {
-                    list = list + "show name: " + response.shows[i].showName + " | time: " + response.shows[i].showTime + " | date:  " + response.shows[i].showDate + " | <mark>available</mark> | " + "showID: " + response.shows[i].showID + "<br>";
+                    list = list + "show name: " + response.shows[i].showName + " | time: " + response.shows[i].showTime + " | date:  " + dateString + " | <mark>available</mark> | " + "showID: " + response.shows[i].showID + " | venue: " + response.shows[i].venueName +"<br>";
                 }
             }
             console.log(list);
